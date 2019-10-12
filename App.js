debb7bc7154e8e334483cfb418c3cc48e938c473
wilhh <wilsonhartantohasan>
2019-10-12 12:43:22 +0800

    Print from Firestore

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import MainNavigator from "./navigation/MainNavigator";
 import firebase from 'firebase';
+import 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyBvSYNhzz71ndyMv1sev-DjCrvDyC8zIxs",
@@ -13,8 +14,29 @@ const firebaseConfig = {
     measurementId: "G-SR4CM03XTV"
   };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+//const firebaseApp = firebase.initializeApp(firebaseConfig);
 //const Database = firebaseApp.database();
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+
+console.log("Welcome to the Thunderdome.");
+
+// Getting Data
+db.collection('scores').get().then((snapshot) => {
+  snapshot.docs.forEach(doc => {
+    //console.log(doc.data())
+    var NRIC = doc.data().NRIC;
+    var accuracy = doc.data().accuracy;
+    var completionTime = doc.data().completionTime;
+    var date = doc.data().date;
+    var noOfErrors = doc.data().noOfErrors;
+    console.log(doc.data().NRIC);
+    console.log(doc.data().accuracy);
+    console.log(doc.data().completionTime);
+    console.log(doc.data().date.toDate());
+    console.log(doc.data().noOfErrors);
+  }) 
+})
 
 const App = () => <MainNavigator/>
 
